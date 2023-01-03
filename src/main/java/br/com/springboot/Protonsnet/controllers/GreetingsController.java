94dbe98d7f16d899ddbb6bb42f6e94758aad587f
unknown <protonsnet@gmail.com>
2023-01-03 17:26:18 -0300

    Mini aplicação completa Java Web com Spring Boot

--- src/main/java/br/com/springboot/Protonsnet/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/Protonsnet/controllers/GreetingsController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +51,7 @@ public class GreetingsController {
      * listaUsuario
      * Método de listar os usuários da API
      * Retorna uma lista de todos os usuários.
+     * 
      */
     @GetMapping(value = "listatodos")
     @ResponseBody //Retorna os dados parao corpo da resposta
@@ -55,5 +61,65 @@ public class GreetingsController {
     	//Retorna a lista em JSON
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
     	
+    }
+    
+    /*
+     * salvar
+     * Método de salvar os dados do usuário
+     */
+    @PostMapping(value = "salvar")
+    @ResponseBody//Descrição da resposta
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+    	Usuario user = usuarioRepository.save(usuario);
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    }
+    
+    /*
+     * delete
+     * Método que elimina o usuário do banco
+     */
+    @DeleteMapping(value = "delete")
+    @ResponseBody //Retorna a descrição da resposta
+    public ResponseEntity<String> delete(@RequestParam Long iduser){
+    	usuarioRepository.deleteById(iduser);
+    	
+    	return new ResponseEntity<String>("Usuário deletado com sucesso", HttpStatus.OK);
+    }
+    
+    /*
+     * buscarusuario
+     * Procura usuário pelo id
+     */
+    @GetMapping(value = "buscarusuario")
+    @ResponseBody //Retorna a descrição da resposta
+    public ResponseEntity<Usuario> buscarusuario(@RequestParam(name = "iduser") Long iduser){
+    	Usuario usuario = usuarioRepository.findById(iduser).get();
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    }
+    
+    /*
+     * atualizar
+     * Atualiza as modificações nos dados do usuário
+     */
+    @PutMapping(value = "atualizar")
+    @ResponseBody //Retorna a descrição da resposta
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){
+    	if(usuario.getId()==null) {
+    		return new ResponseEntity<String>("Id não informado", HttpStatus.OK);
+    	}
+    	Usuario user = usuarioRepository.saveAndFlush(usuario);
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    }
+    
+    /*
+     * buscarPorNome
+     * Busca usuário por nome
+     */
+    @GetMapping(value = "buscarPorNome")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam(name = "nome") String nome){
+    	List<Usuario> usuario = usuarioRepository.buscarPorNome(nome.trim().toUpperCase());
+    	
+    	return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
     }
 }
--- src/main/java/br/com/springboot/Protonsnet/repository/UsuarioRepository.java
+++ src/main/java/br/com/springboot/Protonsnet/repository/UsuarioRepository.java
@@ -6,12 +6,23 @@
 
 package br.com.springboot.Protonsnet.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.springboot.Protonsnet.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	/*
+	 * buscarPorNome
+	 * Criando a interface de consulta por parte do nome.
+	 * ?1 para um único parâmetro "nome"
+	 */
+	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")
+	List<Usuario> buscarPorNome(String name);
 
 }
